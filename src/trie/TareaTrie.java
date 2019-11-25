8544926e95ffee043865ed86bcf9a4fb7da44946
DiegoHuesos <diegohd_98@hotmail.com>
2019-11-25 11:56:00 -0600

    Reporte

--- src/trie/TareaTrie.java
+++ src/trie/TareaTrie.java
@@ -14,41 +14,49 @@ public class TareaTrie {
     
     public static void main(String[] args) throws FileNotFoundException, InterruptedException{
         
-        try(Scanner sc = new Scanner(new File("words.txt"));){
-            
-            ArrayList<String> list = new ArrayList<>();
-            int cont = 0;
-            while(sc.hasNext()){
-                list.add(sc.next());
-                cont++;
-            }
-            System.out.println("Cantidad de palabras: " + cont);
-            String [] arr = new String[cont];
-            for(int i = 0; i<cont; i++){
-                arr[i] = list.get(i);
-            }
-            
-            
-            long startTime, endTime; 
-            startTime = System.currentTimeMillis();
-            //MergeSort.mergeSort(arr, 0, cont);
-            
-            MergeExperiments me = new MergeExperiments();
-            me.mergeSort(arr);
-            //TimeUnit.SECONDS.sleep(1);
-            
-            endTime = System.currentTimeMillis();
-            System.out.println("Merge: "+( (endTime-startTime) )+" milisegundos");
-            
-            startTime = System.currentTimeMillis();
-            WordTrie.ordenaLexicograficamente(list);
-            //TimeUnit.SECONDS.sleep(1);
-            endTime = System.currentTimeMillis();
-            System.out.println("Trie: "+(endTime-startTime) +" milisegundos");
-            
-        }catch(Exception e){
-            System.out.println("Error: " + e.toString());
-        }
+        //System.out.println("CantPalabras\t Merge\t\t Trie");
+        int num = 10000;
         
+        for(int i =0;i<10; i++){
+            try(Scanner sc = new Scanner(new File("words.txt"));){
+                
+                //LEER Y AGREGAR LAS PALARAS A UNA LISTA
+                ArrayList<String> list = new ArrayList<>();
+                int cont = 0;
+                while(cont < num){
+                    list.add(sc.next());
+                    cont++;
+                }
+                //System.out.print(cont + "\t\t");
+                
+                //PASAR LA LISTA A UN ARREGLO
+                String [] arr = new String[cont];
+                for(int j = 0; j<cont; j++){
+                    arr[j] = list.get(j);
+                }
+
+                //¿TimeUnit.SECONDS.sleep(1);?
+                long startTime, endTime; 
+                    
+                //MERGE_SORT:
+                startTime = System.currentTimeMillis();
+                MergeExperiments me = new MergeExperiments();
+                me.mergeSort(arr);
+                endTime = System.currentTimeMillis();
+                System.out.print((endTime-startTime) +"\t");
+
+                //TRIE_SORT:
+                startTime = System.currentTimeMillis();
+                WordTrie.ordenaLexicograficamente(list);
+                endTime = System.currentTimeMillis();
+                System.out.println((endTime-startTime));
+                
+                //INCREMENTAR LA CANTIDAD DE PALABRAS:
+                num += 2000;
+            }        
+            catch(Exception e){
+                System.out.println("Error: " + e.toString());
+            }
+        }
     }
 }
